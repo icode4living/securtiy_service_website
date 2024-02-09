b6046590d6b8e736b4677f96bdc269beca64dc23
samuelsamafolabi@outlook.com <samuelsamafolabi@outlook.com>
2024-02-09 19:35:40 +0100

    mobile modification

--- js/nav-control.js
+++ js/nav-control.js
@@ -3,6 +3,8 @@ let navList = document.querySelectorAll(".link-item");
 document.addEventListener("onload",()=>{
 alert("window loaded")
 })
+
+const header = document.getElementById("header");
 document.addEventListener("scroll", ()=>{
     //check if user scroll to top
     if(window.scrollY===0){
@@ -18,7 +20,7 @@ document.addEventListener("scroll", ()=>{
       //document.querySelectorAll("link-item").classList.replace("link-item-white","link-item");
       navList.forEach((list)=>{
         list.classList.replace("link-item", "link-item-white");
-             console.log("Not scrolling")
+            // console.log("Not scrolling")
 
     })
 
@@ -26,3 +28,29 @@ document.addEventListener("scroll", ()=>{
     }
   
   });
+
+  /**Mobile menu control */
+ var menuButton = document.getElementById("menu-btn");
+ //close button
+ var closeBtn = document.getElementById("close-btn");
+ //open menu on mobile screen
+menuButton.addEventListener("click",()=>{
+    //console.log("menu clicked")
+    closeBtn.style.display = "block";
+document.querySelector(".nav-link").classList.replace("nav-link","nav-menu");
+document.querySelectorAll(".link-item").forEach((list)=>{
+list.classList.replace("link-item","mobile-menu-list");
+})
+
+});
+//close menu on mobile screen
+
+closeBtn.addEventListener("click",()=>{
+    //console.log("menu clicked")
+    closeBtn.style.display = "none";
+document.querySelector(".nav-menu").classList.replace("nav-menu","nav-link");
+document.querySelectorAll(".mobile-menu-list").forEach((list)=>{
+list.classList.replace("mobile-menu-list","link-item");
+})
+
+});
